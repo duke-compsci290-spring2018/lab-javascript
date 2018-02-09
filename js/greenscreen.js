a1ebfa6865681d718c54585e906de21836ef60dc
Robert Duvall <rcd@cs.duke.edu>
2018-02-08 21:17:27 -0500

    Completed green screen exercise

--- js/greenscreen.js
+++ js/greenscreen.js
@@ -9,18 +9,64 @@ function clearCanvas(canvas) {
     context.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-function greenScreen(threshold) {
-    // given foreground and background images, make a new image of the canvas size
-
-    // For each pixel in foreground image
-
-        // if current pixel is green enough,
-
-            // set output image's corresponding pixel to bgImage's pixel at same position
-
-        // Otherwise,
-
-            // set output image's corresponding pixel to current pixel
-
-    // return resulting image
+// load foreground and background images using an file chooser input element and draw it using SimpleImage class
+function loadForeground() {
+    var file = document.getElementById('fgInput');
+    fgImage = new SimpleImage(file);
+    fgImage.drawTo(fgCanvas);
 }
+function loadBackground() {
+    var file = document.getElementById('bgInput');
+    bgImage = new SimpleImage(file);
+    bgImage.drawTo(bgCanvas);
+}
+
+// verify that both images are ready and are the same size before calling the main function
+function greenScreen() {
+    // check that the images are loaded
+    if (fgImage == null  || ! fgImage.complete() || bgImage == null || ! bgImage.complete()) {
+        alert('Image not loaded');
+    }
+    else {
+        // ensure both images are the same size
+        fgImage.setSize(fgCanvas.width, fgCanvas.height);
+        bgImage.setSize(fgCanvas.width, fgCanvas.height);
+        // choose the threshold dynamically
+        var threshold = document.getElementById('threshold').value;
+        greenScreenImage(threshold).drawTo(fgCanvas);
+    }
+}
+function greenScreenImage(threshold) {
+    var result = new SimpleImage(fgCanvas.width, fgCanvas.height);
+    result.pixels().forEach(function(pixel) {
+        var fgPixel = fgImage.getPixel(pixel.getX(), pixel.getY());
+        var bgPixel = bgImage.getPixel(pixel.getX(), pixel.getY());
+        if (fgPixel.getGreen() >= threshold) {
+            pixel.setAllFrom(bgPixel);
+        }
+        else{
+            pixel.setAllFrom(fgPixel);
+        }
+    });
+    return result;
+}
+
+
+// global variables
+var fgImage = null;
+var bgImage = null;
+var fgCanvas = document.getElementById('fgCanvas');
+var bgCanvas = document.getElementById('bgCanvas');
+
+// connect specific events to functions that will respond
+document.getElementById('fgInput').addEventListener('change', loadForeground, false);
+document.getElementById('bgInput').addEventListener('change', loadBackground, false);
+document.getElementById('threshold').addEventListener('input', greenScreen, false);
+document.getElementById('create').addEventListener('click', () => {
+    greenScreen();
+    clearCanvas(bgCanvas);
+}, false);
+document.getElementById('clear').addEventListener('click', () => {
+    clearCanvas(fgCanvas);
+    clearCanvas(bgCanvas);
+}, false);
